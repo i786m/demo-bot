da1c586f2224641d9439c236082ecd0d19ef7e3e
Imran Mohamed <info@imranbmohamed.com>
2026-03-06 13:07:22 +0000

    Implement core functionality to send message to slack and enhanced logging

--- src/config/workspaces.mjs
+++ src/config/workspaces.mjs
@@ -1,18 +1,24 @@
-export const workspaces = {
-	i786m: {
-		token: process.env.SLACK_BOT_DEV_TOKEN,
-		channelId: process.env.SLACK_DEV_CHANNEL_ID,
-	},
-	ITD: {
-		token: process.env.SLACK_BOT_ITD_TOKEN,
-		channelId: process.env.SLACK_ITD_CHANNEL_ID,
-	},
-	ITP: {
-		token: process.env.SLACK_BOT_ITP_TOKEN,
-		channelId: process.env.SLACK_ITP_CHANNEL_ID,
-	},
-	CYF: {
-		token: process.env.SLACK_BOT_CYF_TOKEN,
-		channelId: process.env.SLACK_CYF_CHANNEL_ID,
-	},
-};
+export function getWorkspaces() {
+	return {
+		i786m: {
+			name: 'i786m',
+			token: process.env.SLACK_BOT_DEV_TOKEN,
+			channelId: process.env.SLACK_DEV_CHANNEL_ID,
+		},
+		ITD: {
+			name: 'ITD',
+			token: process.env.SLACK_BOT_ITD_TOKEN,
+			channelId: process.env.SLACK_ITD_CHANNEL_ID,
+		},
+		ITP: {
+			name: 'ITP',
+			token: process.env.SLACK_BOT_ITP_TOKEN,
+			channelId: process.env.SLACK_ITP_CHANNEL_ID,
+		},
+		CYF: {
+			name: 'CYF',
+			token: process.env.SLACK_BOT_CYF_TOKEN,
+			channelId: process.env.SLACK_CYF_CHANNEL_ID,
+		},
+	};
+}
--- src/logs/botLogger.mjs
+++ src/logs/botLogger.mjs
@@ -1,13 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 
-const logFilePath = path.join(process.cwd(), 'bot.log');
+const logFilePath = path.join(process.cwd(), './src/logs/bot.log');
 
 /**
  * Logs a message to the bot.log file with a timestamp.
  * @param {string} message - The message to log
  */
 export function botLogger(message) {
-	const logMessage = `${new Date().toISOString()} - ${message}\n`;
+	const logMessage = `${new Date().toUTCString()}\n${message}\n\n`;
 	fs.appendFileSync(logFilePath, logMessage);
 }
